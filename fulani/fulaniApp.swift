955042284c422aa1bbe2840d9c0167686744de26
SarrMouhamadu <sarrmahmoud232@gmail.com>
2026-07-30 14:01:50 +0000

    Initial Commit

---
+++ fulani/fulaniApp.swift
@@ -0,0 +1,17 @@
+//
+//  fulaniApp.swift
+//  fulani
+//
+//  Created by Mouhamadou SARR on 30-07-2026.
+//
+
+import SwiftUI
+
+@main
+struct fulaniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
